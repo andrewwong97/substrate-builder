5f337ef0296aeb6d77409b1587b4c3f17a2be4eb
Andrew Wong <wongandrew97@gmail.com>
2024-06-24 02:35:09 -0400

    Set up blob storage and azure

--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
 // index.js
+const azure = require('azure-storage');
+const blobService = azure.createBlobService(process.env.AZURE_URL);
 
 const express = require('express');
 const app = express();
@@ -6,6 +8,31 @@ const port = 3001;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.get('/healthcheck', (req, res) => {
+  // Create a container
+  blobService.createContainerIfNotExists('mycontainer', { publicAccessLevel: 'blob' }, (error, result, response) => {
+    if (error) {
+      console.error('Error creating container:', error);
+    } else {
+      console.log('Created?', result.created);
+    }
+  });
+  // List containers
+  let containers = []
+  blobService.listContainersSegmented(null, (error, result, response) => {
+    if (error) {
+      console.error('Error listing containers:', error);
+    } else {
+      console.log('Containers:');
+      containers = result.entries.map(container => container.name);
+      result.entries.forEach(container => {
+        console.log('-', container.name);
+      });
+    }
+  });
+  res.send('Done ' + containers.join(', '));
 });
 
 app.listen(port, () => {
